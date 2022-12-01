570e9522f8cbbb6ea770cd0146cb95b464a564d4
gabilgg07 <gabilgg07@gmail.com>
2022-12-01 17:45:17 +0400

    Add and Design ShopController.Apply Routing Configuration

---
+++ Ogani/Ogani.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Ogani.WebUI.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult ShoppingCard()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
+
--- Ogani/Ogani.WebUI/Startup.cs
+++ Ogani/Ogani.WebUI/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,6 +10,11 @@ namespace Ogani.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddRouting(cfg =>
+            {
+                cfg.LowercaseUrls = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
