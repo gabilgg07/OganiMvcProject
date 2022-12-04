791b3c09c5748dd82d2aa254a8cd677d2cfa96f9
gabilgg07 <gabilgg07@gmail.com>
2022-12-04 17:49:48 +0400

    Cross Join, Dublicate products with sql queries

--- Ogani/Ogani.WebUI/Controllers/ShopController.cs
+++ Ogani/Ogani.WebUI/Controllers/ShopController.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Ogani.WebUI.Models.DataContext;
+using Ogani.WebUI.Models.Entity;
 
 namespace Ogani.WebUI.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        readonly OganiDbContext db;
+
+        public ShopController(OganiDbContext db)
         {
-            return View();
+            this.db = db;
         }
 
-        public IActionResult Details()
+        public IActionResult Index()
         {
-            return View();
+            // lazy loading
+            //List<Product> products = db.Products.ToList();
+            List<Product> products = db.Products
+                .Include(p => p.Images)
+                .ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = db.Products
+                .Include(p => p.Images)
+                .Include(p => p.Unit)
+                .FirstOrDefault(p => p.Id == id);
+
+            return View(product);
         }
 
         public IActionResult ShoppingCard()
